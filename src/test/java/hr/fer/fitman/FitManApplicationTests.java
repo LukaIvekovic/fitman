69289d0ea5aec8f40ea091edc0bcde7a1dfecb09
Luka <luka.ivekovic2@gmail.com>
2025-05-22 20:28:44 +0200

    first commit

---
+++ src/test/java/hr/fer/fitman/FitManApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.fitman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
